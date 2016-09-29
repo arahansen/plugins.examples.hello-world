24cdd11d34f10261d631a026076de25eb1bb0e25
Sam Deane <sam@elegantchaos.com>
2016-09-29 11:12:52 +0100

    Renamed resizeToFitContents to adjustToFit for consistency with groups.

--- Hello World.sketchplugin/Contents/Sketch/hello-world.js
+++ Hello World.sketchplugin/Contents/Sketch/hello-world.js
@@ -68,7 +68,7 @@ function onRun(context) {
   var layer = page.newText({alignment: NSTextAlignmentCenter, systemFontSize: 36, text:"Hello World"})
 
   // ...resize it so that the text just fits...
-  layer.resizeToFitContents()
+  layer.adjustToFit()
 
   // Finally, lets center the view on our new layer
   // so that we can see where it is.
